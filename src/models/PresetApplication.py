ad429c8cb10de3ca620f32935f15837f8151aa18
Ravdeep Singh <ravdeeps3@gmail.com>
2026-05-24 10:59:40 -0400

    feat(models): implement UserImage model and relation to PresetApplications: create src/models/PresetApplication.py

--- src/models/PresetApplication.py
+++ src/models/PresetApplication.py
@@ -1,4 +1,11 @@
+from typing import Optional
+
 class PresetApplication:
-    def __init__(self, preset_id: str):
-        self.preset_id = preset_id
-        self.user_image = None  # Reference to UserImage
+    def __init__(self, name: str, user_image: Optional['UserImage'] = None):
+        self.name = name
+        self.user_image = user_image
+        if user_image:
+            user_image.add_preset_application(self)
+
+    def __repr__(self):
+        return f"<PresetApplication name={self.name} user_image_asset_id={self.user_image.asset_id if self.user_image else None}>"
